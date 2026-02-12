5edde91f1b8f5450b7e826f6a22014b3cd7fbd3b
Jordan Fitoussi <jordanfitoussi@JordanM4.local>
2026-02-12 12:27:45 +0100

    v12: fix iOS zoom

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;9..40,500;9..40,600;9..40,700&family=Instrument+Sans:wght@400;500;600;700&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
